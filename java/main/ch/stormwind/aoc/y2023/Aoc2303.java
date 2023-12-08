12326e8c61ef717dac4d56517de47dc66d814ea9
Claude Nobs <claude.nobs@bedag.ch>
2023-12-09 00:59:58 +0100

    2023-03 unified solution

--- java/main/ch/stormwind/aoc/y2023/Aoc2303.java
+++ java/main/ch/stormwind/aoc/y2023/Aoc2303.java
@@ -3,10 +3,10 @@ package ch.stormwind.aoc.y2023;
 import ch.stormwind.aoc.BaseAocProcessor;
 
 import java.util.ArrayList;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static ch.stormwind.aoc.AocProcessor.Part.PART1;
+import static ch.stormwind.aoc.AocProcessor.Part.PART2;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
@@ -26,35 +26,21 @@ public class Aoc2303 extends BaseAocProcessor<Integer> {
         var lines = input.split("\n");
         var result = 0;
         for (int i = 0; i < lines.length; i++) {
-            var m = (part == PART1 ? number : gear).matcher(lines[i]);
-            while (m.find()) {
-                result += part == PART1 ? part1(lines, i, m) : part2(lines, i, m);
+            var cm = (part == PART1 ? number : gear).matcher(lines[i]);
+            while (cm.find()) {
+                var ns = new ArrayList<Integer>(2);
+                for (int y = max(0, i - 1); y < min(i + 2, lines.length); y++) {
+                    var om = (part == PART1 ? symbol : number).matcher(lines[y]);
+                    while (om.find()) {
+                        if (part == PART1 && om.end() <= cm.end() + 1 && om.start() >= cm.start() - 1)
+                            result += Integer.valueOf(cm.group());
+                        if (part == PART2 && om.start() <= cm.start() + 1 && om.end() >= cm.end() - 1)
+                            ns.add(Integer.valueOf(om.group()));
+                    }
+                }
+                if (part == PART2) result += ns.size() == 2 ? ns.get(0) * ns.get(1) : 0;
             }
         }
         return result;
-    }
-
-    private int part1(String[] lines, int i, Matcher m) {
-        for (int y = max(0, i - 1); y < min(i + 2, lines.length); y++) {
-            for (int x = max(0, m.start() - 1); x < min(m.end() + 1, lines[i].length()); x++) {
-                var c = lines[y].substring(x, x + 1);
-                if (symbol.matcher(c).matches()) {
-                    return Integer.valueOf(m.group(0));
-                }
-            }
-        }
-        return 0;
-    }
-
-    private int part2(String[] lines, int i, Matcher m) {
-        var ns = new ArrayList<Integer>();
-        for (int y = max(0, i - 1); y < min(i + 2, lines.length); y++) {
-            var n = number.matcher(lines[y]);
-            while (n.find()) {
-                if (n.start() <= m.start() + 1 && n.end() >= m.start())
-                    ns.add(Integer.valueOf(n.group(0)));
-            }
-        }
-        return ns.size() == 2 ? ns.get(0) * ns.get(1) : 0;
     }
 }
