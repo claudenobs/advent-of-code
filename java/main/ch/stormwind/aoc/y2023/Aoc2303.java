3e7adb67ab5e4df9b5461b543e37dce0e9fa08da
Claude Nobs <claude.nobs@bedag.ch>
2023-12-09 01:00:30 +0100

    2023-03: even more unified

--- java/main/ch/stormwind/aoc/y2023/Aoc2303.java
+++ java/main/ch/stormwind/aoc/y2023/Aoc2303.java
@@ -2,11 +2,9 @@ package ch.stormwind.aoc.y2023;
 
 import ch.stormwind.aoc.BaseAocProcessor;
 
-import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import static ch.stormwind.aoc.AocProcessor.Part.PART1;
-import static ch.stormwind.aoc.AocProcessor.Part.PART2;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
@@ -28,17 +26,19 @@ public class Aoc2303 extends BaseAocProcessor<Integer> {
         for (int i = 0; i < lines.length; i++) {
             var cm = (part == PART1 ? number : gear).matcher(lines[i]);
             while (cm.find()) {
-                var ns = new ArrayList<Integer>(2);
+                int first = -1; // only used for PART2
                 for (int y = max(0, i - 1); y < min(i + 2, lines.length); y++) {
                     var om = (part == PART1 ? symbol : number).matcher(lines[y]);
                     while (om.find()) {
-                        if (part == PART1 && om.end() <= cm.end() + 1 && om.start() >= cm.start() - 1)
-                            result += Integer.valueOf(cm.group());
-                        if (part == PART2 && om.start() <= cm.start() + 1 && om.end() >= cm.end() - 1)
-                            ns.add(Integer.valueOf(om.group()));
+                        var c = part == PART1 ? cm : om; var o = part == PART1 ? om : cm;
+                        if (o.end() <= c.end() + 1 && o.start() >= c.start() - 1) {
+                            var n = Integer.parseInt(c.group());
+                            if (part == PART1) { result += n; }
+                            else if (first < 0) { first = n; }
+                            else { result += first * n; first = -1; }
+                        }
                     }
                 }
-                if (part == PART2) result += ns.size() == 2 ? ns.get(0) * ns.get(1) : 0;
             }
         }
         return result;
